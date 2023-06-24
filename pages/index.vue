c202009ac20cacee83bf3c165e3afb0ccaf4d78e
ikun <0215.miyatto@gmail.com>
2023-06-24 11:03:30 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Hello Nuxt.js</h1>
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  async asyncData({ $axios }) {
+    try {
+      const response = await $axios.$get('/api/posts');
+      return {
+        message: response.message
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
+}
+</script>
